706a9e081cd83228629f8cbfd417cb62030db019
Empressfavs <favouradaavwopiabro@gmail.com>
2024-10-25 12:49:35 +0100

    files restored

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
